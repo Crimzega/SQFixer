83ff025f045c39d9654af30bd5c388a92d0423bf
Crimzega <tpodCI@gmail.com>
2021-12-16 23:39:27 -0600

    Update Patch

--- src/main/java/com/sulvic/sqfixer/ReferenceSQF.java
+++ src/main/java/com/sulvic/sqfixer/ReferenceSQF.java
@@ -2,6 +2,6 @@ package com.sulvic.sqfixer;
 
 public class ReferenceSQF{
 	
-	public static final String MODID = "sqfixer", NAME = "SpiderQueen Fixer", VERSION = "1.0", DEPENDENCIES = "required-after:SQ@[1-7-10-1.1.0,)";
+	public static final String MODID = "sqfixer", NAME = "SpiderQueen Fixer", VERSION = "1.1", DEPENDENCIES = "required-after:SQ@[1-7-10-1.1.0,)";
 	
 }
