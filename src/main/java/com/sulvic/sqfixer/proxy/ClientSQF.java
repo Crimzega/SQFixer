b794febe514f8eb664026a48bffc6bd75c0c711c
Crimzega Sulvic <tpodCI@gmail.com>
2023-04-21 15:56:17 -0500

    Removing Custom Cocoon Model Rendering

--- src/main/java/com/sulvic/sqfixer/proxy/ClientSQF.java
+++ src/main/java/com/sulvic/sqfixer/proxy/ClientSQF.java
@@ -1,17 +1,14 @@
 package com.sulvic.sqfixer.proxy;
 
-import com.sulvic.sqfixer.client.render.RenderCocoonNew;
 import com.sulvic.sqfixer.client.render.RenderHumanFix;
 
 import cpw.mods.fml.client.registry.RenderingRegistry;
-import sq.entity.creature.EntityCocoon;
 import sq.entity.creature.EntityHuman;
 
 public class ClientSQF extends ServerSQF{
 
 	public void registerRenders(){
 		RenderingRegistry.registerEntityRenderingHandler(EntityHuman.class, new RenderHumanFix());
-		RenderingRegistry.registerEntityRenderingHandler(EntityCocoon.class, new RenderCocoonNew());
 	}
 
 }
